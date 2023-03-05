0559bb16d56b6730d448f946058bc596201d1e23
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-05 14:25:46 +0200

    EditNoteView

---
+++ lib/presentation/screens/edit_note_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
