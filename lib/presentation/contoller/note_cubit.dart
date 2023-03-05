cd6225261d914909376793ea90c00f8477864702
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-03-05 16:44:04 +0200

    add BlocConsumer to homeview

--- lib/presentation/contoller/note_cubit.dart
+++ lib/presentation/contoller/note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:note/data/models/note_mode.dart';
 
@@ -10,11 +11,15 @@ class NoteCubit extends Cubit<NoteState> {
 
   final NotesRepository _notesRepository;
 
+  static NoteCubit get(context) => BlocProvider.of(context);
+
   List<NoteModel>? noteModel;
   Future<void> getNotes() async {
     emit(GetNotesLoadingState());
     await _notesRepository.getNotes().then((value) {
       noteModel = value;
+      debugPrint(value.length.toString());
+      debugPrint(value.toString());
       emit(GetNotesSuccessState());
     }).catchError((onError) {
       emit(GetNotesErrorState());
--- lib/presentation/screens/home_view.dart
+++ lib/presentation/screens/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note/presentation/contoller/note_cubit.dart';
 
 import '../widgets/custom_app_bar.dart';
 import '../widgets/notes_list_view.dart';
@@ -9,21 +11,31 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              CustomAppBar(
-                title: 'Note',
-                onPressedIcon: () {},
-                icon: Icons.search,
+      child: BlocConsumer<NoteCubit, NoteState>(
+        listener: (context, state) {
+          // TODO: implement listener
+        },
+        builder: (context, state) {
+          var cubit = NoteCubit.get(context);
+          return Scaffold(
+            body: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  CustomAppBar(
+                    title: 'Note',
+                    onPressedIcon: () {},
+                    icon: Icons.search,
+                  ),
+                  const SizedBox(height: 30),
+                  NotesListView(
+                    noteCubit: cubit,
+                  ),
+                ],
               ),
-              const SizedBox(height: 30),
-              const NotesListView(),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
